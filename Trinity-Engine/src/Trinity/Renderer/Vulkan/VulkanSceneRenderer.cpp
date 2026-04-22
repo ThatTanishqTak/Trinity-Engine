62c953c5c1e6a031ea8feffed89d94a052832439
ThatTanishqTak <tanishqtak3@gmail.com>
2026-04-22 21:10:48 +0100

    Created VulkanSceneRenderer files and refactor

--- Trinity-Engine/src/Trinity/Renderer/Vulkan/VulkanSceneRenderer.cpp
+++ Trinity-Engine/src/Trinity/Renderer/Vulkan/VulkanSceneRenderer.cpp
@@ -1,5 +1,4 @@
-#include "Trinity/Renderer/ISceneRenderer.h"
-#include "Trinity/Renderer/SceneRenderer.h"
+#include "Trinity/Renderer/Vulkan/VulkanSceneRenderer.h"
 
 #include "Trinity/Renderer/Renderer.h"
 #include "Trinity/Renderer/Mesh.h"
@@ -28,33 +27,6 @@
 
 namespace Trinity
 {
-    class VulkanSceneRenderer final : public ISceneRenderer
-    {
-    public:
-        VulkanSceneRenderer();
-        ~VulkanSceneRenderer() override;
-
-        void Initialize(uint32_t width, uint32_t height) override;
-        void Shutdown() override;
-        void BeginScene(const Camera& camera, const SceneRenderData& sceneData) override;
-        void SubmitMesh(const MeshDrawCommand& command) override;
-        void EndScene() override;
-        void Render() override;
-        void OnResize(uint32_t width, uint32_t height) override;
-
-        std::shared_ptr<Texture> GetFinalOutput() const override;
-        const SceneRendererStats& GetStats() const override;
-
-    private:
-        struct Implementation;
-        std::unique_ptr<Implementation> m_Implementation;
-        std::vector<MeshDrawCommand> m_DrawList;
-        Camera m_Camera;
-        SceneRenderData m_SceneData;
-        uint32_t m_Width = 0;
-        uint32_t m_Height = 0;
-    };
-
     struct LightingUniformData
     {
         float InvViewProjection[16];
